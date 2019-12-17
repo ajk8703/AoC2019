b172b52a00526fc4404e8f8c35778e3d9d20de8d
ajk8703 <ajkim8703@gmail.com>
2019-12-17 11:34:03 -0500

    I'm tryingggggg

---
+++ aoc2019/src/day3/Day3_C1.java
@@ -0,0 +1,13 @@
+package day3;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Day3_C1 {
+	public static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static int[] getDir(char c) {
+		switch (c):
+			case 'L' 
+	}
+
+}
